0ee1b58148a65ca9ccdd5a4016cd181fa3168f96
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-09 04:43:28 +0900

    feat: SignIn 내부 Form 추가
    기본 골격 잡음 (빈 핸들러)
    로그인 로직 추가 필요

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,23 @@
-interface SignInProps {}
+import { Link } from 'react-router-dom'
+import SignForm from '../components/SignForm'
 
-const SignIn = ({}: SignInProps) => {
-  return <div>SignIn</div>
+const SignIn = () => {
+  const SignInSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+
+    // TODO:
+    // 로그인 API 호출
+    // 성공하면 /todo로 이동
+    // 실패하면 alert
+  }
+
+  return (
+    <>
+      <SignForm title='로그인' handleSubmit={SignInSubmitHandler} />
+      <Link to='/signup'>회원가입 하러가기</Link>
+    </>
+  )
 }
 
 export default SignIn
