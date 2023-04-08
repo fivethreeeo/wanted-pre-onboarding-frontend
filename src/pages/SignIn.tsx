61f9f062916aed42c0ab16908479bbbe4fbc1bea
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-09 04:44:00 +0900

    feat: 로그인 기능 완성 (로컬서버, api 호출, "/todo" 경로 이동)
    api 엔드포인트 변경 필요

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,15 +1,50 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import SignForm from '../components/SignForm'
+import { saveAccessTokenToLocalStorage } from '../utils/accessTokenHandler'
+
+interface SignInRequest {
+  email: string
+  password: string
+}
+
+type SignInResult = 'success' | 'fail'
+
+const signin = async (args: SignInRequest): Promise<SignInResult> => {
+  // TODO: api 엔드포인트 변경 필요
+  const signInRes = await fetch('http://localhost:8000/auth/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(args),
+  })
+
+  if (signInRes.ok) {
+    const { access_token } = await signInRes.json()
+    saveAccessTokenToLocalStorage(access_token)
+    return 'success'
+  }
+
+  return 'fail'
+}
 
 const SignIn = () => {
+  const navigate = useNavigate()
+
   const SignInSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
 
-    // TODO:
-    // 로그인 API 호출
-    // 성공하면 /todo로 이동
-    // 실패하면 alert
+    signin({
+      email: formData.get('text') as string,
+      password: formData.get('password') as string,
+    }).then(res => {
+      if (res === 'success') {
+        navigate('/todo')
+      } else {
+        alert('로그인에 실패했습니다.')
+      }
+    })
   }
 
   return (
