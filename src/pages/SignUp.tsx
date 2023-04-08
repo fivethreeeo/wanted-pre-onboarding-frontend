ce494cbaad556f5984466baf9740e2045133f32a
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-09 04:10:50 +0900

    feat: 회원가입 기능 완성 (로컬서버, api 호출, "/signin" 경로 이동)
    api 엔드포인트 변경 필요

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,16 +1,44 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import SignForm from '../components/SignForm'
 
+interface SignUpRequest {
+  email: string
+  password: string
+}
+
+type SignUpResult = 'success' | 'fail'
+
+const signup = async (args: SignUpRequest): Promise<SignUpResult> => {
+  // TODO: api 엔드포인트 변경 필요
+  const signUpRes = await fetch('http://localhost:8000/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(args),
+  })
+
+  return signUpRes.ok ? 'success' : 'fail'
+}
+
 const SignUp = () => {
+  const navigate = useNavigate()
+
   const signUpSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     const formData = new FormData(event.currentTarget)
 
-    // TODO:
-    // 회원가입 API 호출
-    // 회원가입 성공 시 /signin 으로 이동
-    // 회원가입 실패 시 에러 메시지 출력 (alert)
+    signup({
+      email: formData.get('text') as string,
+      password: formData.get('password') as string,
+    }).then(res => {
+      if (res === 'success') {
+        navigate('/signin')
+      } else {
+        alert('회원가입에 실패했습니다.')
+      }
+    })
   }
 
   return (
