a14ac269d9132eb2c4cd73be95dc35fab17b1c61
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-09 04:08:43 +0900

    feat: SignUp 내부 Form 추가
    기본 골격 잡음 (빈 핸들러)
    회원가입 로직 추가 필요

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,24 @@
-interface SignUpProps {}
+import { Link } from 'react-router-dom'
+import SignForm from '../components/SignForm'
 
-const SignUp = ({}: SignUpProps) => {
-  return <div>SignUp</div>
+const SignUp = () => {
+  const signUpSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    // TODO:
+    // 회원가입 API 호출
+    // 회원가입 성공 시 /signin 으로 이동
+    // 회원가입 실패 시 에러 메시지 출력 (alert)
+  }
+
+  return (
+    <>
+      <SignForm title='회원가입' handleSubmit={signUpSubmitHandler} />
+      <Link to='/signin'>로그인 하러가기</Link>
+    </>
+  )
 }
 
 export default SignUp
