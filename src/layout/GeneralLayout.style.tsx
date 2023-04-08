9c0f5c9eec2c02a88206b0368b0a5c14b19b84e1
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-09 01:03:51 +0900

    design: 페이지 공통 스타일 추가 (flex)
    화면 가운데 배치

---
+++ src/layout/GeneralLayout.style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const GeneralLayoutContainer = styled.div`
+  padding: 0 24px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`
+
+export { GeneralLayoutContainer }
--- src/layout/GeneralLayout.tsx
+++ src/layout/GeneralLayout.tsx
@@ -1,9 +1,21 @@
+import { GeneralLayoutContainer } from './GeneralLayout.style'
+
 interface GeneralLayoutProps {
   children: React.ReactNode
 }
 
 const GeneralLayout: React.FC<GeneralLayoutProps> = ({ children }) => {
-  return <div className='general-layout'>{children}</div>
+  // TODO:
+  // 로그인 여부 체크
+  // 로컬스토리지에 토큰이 있는 상태면
+  // ㄴ /signin or /signup => /todo
+  // ㄴ /todo => /todo
+  // 로컬스토리지에 토큰이 없는 상태면
+  // ㄴ /signin => /signin
+  // ㄴ /signup => /signup
+  // ㄴ /todo => /signin
+
+  return <GeneralLayoutContainer>{children}</GeneralLayoutContainer>
 }
 
 export default GeneralLayout
