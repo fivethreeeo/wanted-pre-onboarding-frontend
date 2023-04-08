c01c52112064988af1081fca2ffd629d095a9280
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-08 21:08:06 +0900

    design: GlobalStyles 작성 & 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import GlobalStyles from './styles/GlobalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
