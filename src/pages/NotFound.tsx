e0b04d8f2d78fde728e9a9fd91c2372b8cebc226
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-04-09 21:15:14 +0900

    feat: NotFound 페이지에서 로그인, 회원가입, 투드리스트 경로 링크 제공

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,17 @@
-interface NotFoundProps {}
+import { Link } from 'react-router-dom'
 
-const NotFound = ({}: NotFoundProps) => {
-  return <div>NotFound</div>
+const NotFound = () => {
+  return (
+    <div>
+      <Link to='/signin'>로그인페이지로 이동하기</Link>
+      <br />
+      <br />
+      <Link to='/signup'>회원가입페이지로 이동하기</Link>
+      <br />
+      <br />
+      <Link to='/todo'>투두리스트페이지로 이동하기</Link>
+    </div>
+  )
 }
 
 export default NotFound
